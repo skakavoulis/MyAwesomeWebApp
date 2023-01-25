6e1ea27be28d1aff4963bbbe616afdbd58e61457
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2023-01-25 18:08:49 +0200

    Add global Exception Handler

--- Program.cs
+++ Program.cs
@@ -1,11 +1,39 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseExceptionHandler("/Home/Error");
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = Application.Json;
+
+        var feature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var error = feature?.Error;
+
+        if (error is ApplicationException)
+        {
+            await context.Response.WriteAsJsonAsync(new
+            {
+                Error = error.Message
+            });
+        }
+        else
+        {
+            await context.Response.WriteAsJsonAsync(new
+            {
+                Error = "Something went wrong. Please try again later."
+            });
+        }
+    });
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
